bb392dc8ccba0f41a8b22eaef7b29a1dd3bb76d4
zhangchunlong <clzhang@adobe.com>
2026-01-05 11:32:16 -0800

    update css and js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,12 +1,23 @@
 /* /blocks/teaser/teaser.js */
 
 /**
- * Adds a zoom effect to image using event listeners.
+ * Bock options are applied as classes to the block's DOM
+ * element along side the `block` and `<block-name>` classes.
  *
- * When the CTA button is hovered over, the image zooms in.
- *
- * @param {HTMLElement} block represents the block's' DOM tree
- */
+ * @param {HTMLElement} block represents the block's' DOM element/tree
+ * */
+function getOptions(block) {
+  // Get the block's classes, excluding 'block' and 'teaser'.
+  return [...block.classList].filter((c) => !['block', 'teaser'].includes(c));
+}
+
+/**
+  * Adds a zoom effect to image using event listeners.
+  *
+  * When the CTA button is hovered over, the image zooms in.
+  *
+  * @param {HTMLElement} block represents the block's' DOM tree
+  */
 function addEventListeners(block) {
   block.querySelector('.button').addEventListener('mouseover', () => {
     block.querySelector('.image').classList.add('zoom');
@@ -18,40 +29,36 @@ function addEventListeners(block) {
 }
 
 /**
-     * Entry point to block's JavaScript.
-     * Must be exported as default and accept a block's DOM element.
-     * This function is called by the project's style.js, and passed the block's element.
-     *
-     * @param {HTMLElement} block represents the block's' DOM element/tree
-     */
+   * Entry point to block's JavaScript.
+   * Must be exported as default and accept a block's DOM element.
+   * This function is called by the project's style.js, and passed the block's element.
+   *
+   * @param {HTMLElement} block represents the block's' DOM element/tree
+   */
 export default function decorate(block) {
   /* This JavaScript makes minor adjustments to the block's DOM */
 
-  // Dress the DOM elements with semantic CSS classes so it's obvious what they are.
-  // If needed we could also add ARIA roles and attributes, or add/remove/move DOM elements.
-
-  // Add a class to the first picture element to target it with CSS
-  block.querySelector('picture').classList.add('image-wrapper');
-
-  // Use previously applied classes to target new elements
-  block.querySelector('.image-wrapper img').classList.add('image');
-
-  // Mark the second/last div as the content area (white, bottom aligned box w/ text and cta)
+  /* Common treatments for all options */
   block.querySelector(':scope > div:last-child').classList.add('content');
-
-  // Mark the first H1-H6 as a title
   block.querySelector('h1,h2,h3,h4,h5,h6').classList.add('title');
+  block.querySelector('img').classList.add('image');
 
   // Process each paragraph and mark it as text or terms-and-conditions
   block.querySelectorAll('p').forEach((p) => {
     const innerHTML = p.innerHTML?.trim();
-
     if (innerHTML?.startsWith('Terms and conditions:')) {
-      /* If a paragraph starts with '*', add a special CSS class. */
       p.classList.add('terms-and-conditions');
     }
   });
 
-  // Add event listeners to the block
+  /* Conditional treatments for specific options */
+  if (getOptions(block).includes('side-by-side')) {
+    /* For side-by-side teaser, add the image-wrapper a higher-level div to support CSS */
+    block.querySelector(':scope > div:first-child').classList.add('image-wrapper');
+  } else if (!getOptions(block)) {
+    /* For the default option, add the image-wrapper to the picture element to support CSS */
+    block.querySelector('picture').classList.add('image-wrapper');
+  }
+
   addEventListeners(block);
 }
